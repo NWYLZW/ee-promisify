52db5f7a3088cf62744008f3fb1c4985478cfac0
yijie4188 <yijie4188@gmail.com>
2022-11-25 11:48:36 +0800

    feat: unit test

--- index.spec.ts
+++ index.spec.ts
@@ -23,4 +23,29 @@ describe('Event Emiiter Promisify', function () {
     for await (const [a0, a1] of ee.on.bor) {
     }
   })
+  it('should infer event emitter', async () => {
+    const ee = promisify({
+      /**
+       * @return {void}
+       */
+      onfoo: undefined,
+      /**
+       * @param {string} a0
+       * @return {void}
+       */
+      onfuo: undefined,
+      onfue: undefined as (a0: string) => void,
+      /**
+       * @param {number} a0
+       * @param {boolean} [a1]
+       * @return {void}
+       */
+      onfuu: undefined
+    })
+    ee.on('foo', () => {})
+    const [ a01 ] = await ee.on.fuo
+    const [ a02 ] = await ee.on.fue
+    for await (const [a0, a1] of ee.on.fuu) {
+    }
+  })
 })
