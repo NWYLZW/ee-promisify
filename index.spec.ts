5af3b6996de7d2f96e9f8defa053b391c98a72c7
yijie4188 <yijie4188@gmail.com>
2022-12-04 12:39:32 +0800

    feat: unit test type

--- index.spec.ts
+++ index.spec.ts
@@ -1,5 +1,6 @@
 import { expect } from 'chai'
 import promisify from './index'
+import { Equal, Expect } from './type.test'
 
 declare module './index' {
   export interface EventsMap {
@@ -18,34 +19,30 @@ describe('Event Emiiter Promisify', function () {
       onber: undefined,
       onbor: undefined
     }, 'foo')
-    ee.on('bar', () => {})
+    const args = await ee.once.bar
+    type Case0 = Expect<Equal<typeof args, []>>
     const [ a0 ] = await ee.once.ber
-    //      ^?
+    type Case1 = Expect<Equal<typeof a0, string>>
     for await (const [a0, a1] of ee.on.bor) {
-      //              ^?
-      //                  ^2?
+      type Case0 = Expect<Equal<
+        [typeof a0, typeof a1], [number, boolean]
+      >>
     }
   })
   it('should infer event emitter', async () => {
-    //    v?
     const ee = promisify({
       onfoo: undefined as () => void,
       onfuo: undefined as (a0: string) => void,
       onfuu: undefined as (a0: string, a1: number) => void
     })
-
-    // v?
-    ee.on('foo', () => {})
-    // v?
-    ee.on('fuo', a0 => {
-      //         ^?
-    })
-    //                          v?
+    const args = await ee.once.foo
+    type Case0 = Expect<Equal<typeof args, []>>
     const [ a01 ] = await ee.once.fuo
-    //      ^?
+    type Case1 = Expect<Equal<typeof a01, string>>
     for await (const [a0, a1] of ee.on.fuu) {
-      //              ^?
-      //                  ^2?
+      type Case0 = Expect<Equal<
+        [typeof a0, typeof a1], [string, number]
+      >>
     }
   })
 })
