cb4ddfbcee21783940ec5d863dc5c32fe71385eb
yijie4188 <yijie4188@gmail.com>
2022-12-05 10:35:03 +0800

    fix: unable resolve `Symbol.spacies` for `Narrow`

--- src/type.ts
+++ src/type.ts
@@ -8,4 +8,8 @@ export type Cast<A, B> = A extends B ? A : B
 
 export type Primitive = string | number | boolean | bigint | symbol | undefined | null
 
-export type Narrow<T> = Cast<T, [] | (T extends Primitive ? T : never) | ({ [K in keyof T]: Narrow<T[K]> })>
+export type Narrow<T> = Cast<T, [] | (T extends Primitive ? T : never) | ({
+  [K in keyof T]: K extends typeof Symbol.species
+    ? T[K]
+    : Narrow<T[K]>
+})>
